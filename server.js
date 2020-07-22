6b5893e17571cf8f694a47a35660b7c2f3325222
unknown <pawanrealtek@gmail.com>
2020-07-22 16:44:07 +0530

    port changed

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ if(process.env.NODE_ENV === 'production') {
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
-app.listen(5000, () => console.log('server connected'));
+app.listen(process.env.PORT || 5000, () => console.log('server connected'));
